379ed3e80f599633219f482aabda20b887011319
Pobre01 <163008650+Pobre01@users.noreply.github.com>
2024-08-26 10:32:26 -0300

    .

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 const mario = document.querySelector(".mario");
+const pipe = ducument.querySelector(".pipe");
 
 const jump = () => {
     mario.classList.add('jump');
 
+    setInterval(() => {
+        mario.classList.remove("jump");
+    }
+,500 );
 }
+
+const loop = setInterval(() => {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.addEventListener('keydown', jump);
 
-const pipe 
